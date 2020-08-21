67b74aa022b675503ce773b8878635d1e445fc1d
VANG Jeannette <vangjeannette@gmail.com>
2020-08-21 20:31:09 +0200

    basic article list structure

---
+++ client/src/components/ArticleItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ArticleItemComponent extends React.Component {
+  render() {
+    return 'ok';
+  }
+}
+
+export default ArticleItemComponent;
---
+++ client/src/components/ArticlesList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ArticleItemComponent from './ArticleItem';
+
+class ArticlesListComponent extends React.Component {
+  render() {
+    return <ArticleItemComponent />;
+  }
+}
+
+export default ArticlesListComponent;
