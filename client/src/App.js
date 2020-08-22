454eff299b36f3bf94672b18f1e7b9521e28f1e8
VANG Jeannette <vangjeannette@gmail.com>
2020-08-22 21:29:07 +0200

    refacto component display

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import NavbarComponent from './components/NavbarComponent';
-import ArticlesList from './components/ArticlesList';
-import Login from './components/Login';
-import SignUpComponent from './components/SignUp';
+// import ArticlesList from './components/ArticlesList';
+// import Login from './components/Login';
+// import SignUpComponent from './components/SignUp';
 import './App.css';
-import AddArticleComponent from './components/AddArticle';
+// import AddArticleComponent from './components/AddArticle';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
-      <ArticlesList />
-      <Login />
-      <SignUpComponent />
-      <AddArticleComponent />
+      {/* <ArticlesList /> */}
+      {/* <Login /> */}
+      {/* <SignUpComponent /> */}
+      {/* <AddArticleComponent /> */}
     </div>
   );
 }
--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -1,18 +1,95 @@
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav'
+import Nav from 'react-bootstrap/Nav';
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import SignUpComponent from './SignUp';
+import LoginComponent from './Login';
+import ArticlesListComponent from './ArticlesList';
+import AddArticleComponent from './AddArticle';
 class NavbarComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isUserConnected: true,
+      showLoginComponent: false,
+      showSignUpComponent: false,
+      showArticleListComponent: false,
+      showAddArticleComponent: false,
+    };
+  }
+
+  showSignUpComponent = () => {
+    this.setState({ showSignUpComponent: true, showLoginComponent: false });
+  };
+
+  showLoginComponent = () => {
+    this.setState({ showSignUpComponent: false, showLoginComponent: true });
+  };
+
+  showArticleListComponent = () => {
+    this.setState({
+      showArticleListComponent: true,
+      showLoginComponent: false,
+      showSignUpComponent: false,
+    });
+  };
+
+  logout = () => {
+    this.setState({ isUserConnected: false });
+    localStorage.removeItem('token');
+  };
+
+  showAddArticleComponent = () => {
+    this.setState({
+      showLoginComponent: false,
+      showSignUpComponent: false,
+      showAddArticleComponent: true,
+    });
+  };
+
   render() {
     return (
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-      </Navbar>
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Blog</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home" onClick={this.showArticleListComponent}>
+              Home
+            </Nav.Link>
+            {!this.state.isUserConnected && (
+              <div>
+                <Button variant="primary" onClick={this.showSignUpComponent}>
+                  Sign up
+                </Button>{' '}
+                <Button variant="primary" onClick={this.showLoginComponent}>
+                  Login
+                </Button>{' '}
+              </div>
+            )}
+            {this.state.isUserConnected && (
+              <div>
+                <Button variant="primary" onClick={this.logout}>
+                  Log out
+                </Button>{' '}
+                <Button
+                  variant="primary"
+                  onClick={this.showAddArticleComponent}
+                >
+                  Create new article
+                </Button>{' '}
+              </div>
+            )}
+          </Nav>
+        </Navbar>
+        {this.state.showSignUpComponent && <SignUpComponent />}
+        {this.state.showLoginComponent && <LoginComponent />}
+        {this.state.showArticleListComponent &&
+          !this.state.showSignUpComponent &&
+          !this.state.showLoginComponent && <ArticlesListComponent />}
+        {this.state.showAddArticleComponent &&
+          !this.state.showLoginComponent &&
+          !this.state.showSignUpComponent && <AddArticleComponent />}
+      </div>
     );
   }
 }
